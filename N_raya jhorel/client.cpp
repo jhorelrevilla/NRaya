1c359891c6bc392cf0e438bdeb00cbd152fae089
jhorelrevilla <jhorel.revilla@ucsp.edu.pe>
2020-05-10 17:11:05 -0500

    f

---
+++ N_raya jhorel/client.cpp
@@ -0,0 +1,167 @@
+//gcc -std=c++11 client.cpp -lpthread -o client
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+using namespace std;
+
+
+
+struct Casilla{
+    char prop = '*';
+};
+class Raya{
+private:
+    vector<vector<Casilla>> tablero;
+public:
+    int size;
+    int num_jugada = 0;
+    int max_jugadas = size * size; 
+    Raya(int size){
+        this->size = size;
+        tablero.resize(size, vector<Casilla>(size));
+    }
+    void ImprimirTablero(){
+        int x = 0;
+        string top = "  ";
+        for (int i = 0; i < size; i++){top+= to_string(i)+" ";}
+        top+="\n";
+        cout << top;
+        for (auto i = tablero.begin(); i != tablero.end(); ++i){
+            cout << x << " ";x++;
+            for( auto j = i->begin(); j != i->end(); ++j)           
+                cout << j->prop<<" ";          
+            cout << "\n";
+        } 
+    }
+    bool InsertarJugada(char ficha, int x, int y){
+      tablero[x][y].prop = ficha;
+    }
+    bool IsWin(int y, int x, char ficha){
+      int d=0,a=0;
+      for(int i = 0; i < size; i++){
+          if(tablero[y][i].prop ==ficha)d++;
+          if(tablero[i][x].prop ==ficha)a++;
+      }
+      if (d==size || a==size ){return true;}
+      d=0;a=0;
+      if (x==y || (x+y)==size-1){
+          for(int i = 0; i < size; i++){
+              if(tablero[i][i].prop==ficha)d++;
+              if(tablero[i][size-1-i].prop==ficha)a++;
+          }
+          if (d==size || a==size ){return true;}
+      }
+      return false;
+    }
+    int VerificarEstadoJuego(int x, int y, char ficha){
+      if (IsWin(y, x, ficha)){
+          return 1; //Hay ganador
+      }
+      else{
+          if (num_jugada >= max_jugadas-1){
+              //Hay empate
+              return -1;
+          }
+          else{   //Juego en marcha
+              num_jugada++;
+              return 0;
+          }       
+      }
+    }
+    void ReiniciarTablero(){
+      tablero.assign(size, vector<Casilla>(size));
+      num_jugada = 0;
+    }
+    bool verificarPos(int x,int y){
+      cout<<"x "<<x<<" y "<<y<<endl;
+      if ((int)tablero[x][y].prop == (int)'*')
+        return true;
+      return false;
+    }
+    ~Raya(){};
+};
+////////////////////////////////////////////////////////////////////////////////////////
+int size_juego = 3;
+Raya *TresRaya;
+string usuario;
+////////////////////////////////////////////////////////////////////////////////////////
+void RecepcionMensaje(int SocketFD){
+  int n;
+  char buffer[256];
+  int longitud = 0;
+  n = read(SocketFD,buffer,1);
+  if (n < 0) perror("ERROR reading from socket");
+  if (n==1){
+    cout<<usuario<<" Recibi "<<buffer[0]<<" "<<usuario[0]<<endl;
+  }
+  
+  for(;;){
+    n = read(SocketFD,buffer,1);
+    if (n < 0) perror("ERROR reading from socket");
+    if (n == 1){
+      TresRaya->InsertarJugada(buffer[0],(int)(buffer[1]-48),(int)(buffer[2]-48));
+      TresRaya->ImprimirTablero();
+    }
+    bzero(buffer,256);
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////
+void EnvioMensaje(int SocketFD){
+  string msgToChat="";
+  char buffer[256];
+  int n;
+  for(;;){
+      cout << "\nIngrese posiciones: \n";
+      cin>>msgToChat;
+      cout<<"1"<<endl;
+      
+      if(msgToChat!="" && TresRaya->verificarPos((int)msgToChat[0]-48,(int)msgToChat[1]-48 )){
+        cout<<"2"<<endl;
+        msgToChat=usuario+msgToChat;
+        cout<<"envie al server"<<msgToChat<<endl;
+        n = write(SocketFD, msgToChat.c_str(), msgToChat.length());
+        bzero(buffer, 256);
+      } 
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////
+int main(void){
+  struct sockaddr_in stSockAddr;
+  int Res;
+  int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  char buffer[256];
+  int n;
+  if (-1 == SocketFD){perror("cannot create socket");exit(EXIT_FAILURE);}
+  memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+  stSockAddr.sin_family = AF_INET;
+
+  stSockAddr.sin_port = htons(1100);
+  Res = inet_pton(AF_INET, "127.0.0.1", &stSockAddr.sin_addr);
+
+  if (0 > Res){perror("error: first parameter is not a valid address family");close(SocketFD);exit(EXIT_FAILURE);}
+  else if (0 == Res){perror("char string (second parameter does not contain valid ipaddress");close(SocketFD);exit(EXIT_FAILURE);}
+  if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in))){perror("connect failed");close(SocketFD);exit(EXIT_FAILURE);}
+  bzero(buffer, 256);
+  // / / / / / / / / / / / / / / / / / / / / / /
+  //cout<<"Escribe el tamanio del juego \n";
+  //cin>>size_juego;
+  TresRaya=new Raya(size_juego);
+  cout<<"Escoje la ficha que te va a representar: ";
+  getline(cin,usuario);
+  string msg="T"+to_string(size_juego)+"U"+usuario;
+  n=write(SocketFD, msg.c_str(), msg.length());
+  // / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+  std::thread(EnvioMensaje, SocketFD).detach();
+  std::thread(RecepcionMensaje, SocketFD).detach();
+  for(;;){}
+  shutdown(SocketFD, SHUT_RDWR);
+  close(SocketFD);
+  return 0;
+}
---
+++ N_raya jhorel/server.cpp
@@ -0,0 +1,87 @@
+//g++ -std=c++11 server.cpp -lpthread -o server
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <string>
+#include <thread>
+#include <vector>
+#include <map>
+#include <vector>
+using namespace std;
+
+//////////////////////////////////////////////////////////////////////////
+std::vector<pair<char, int>> LISTA_CLIENTES;
+vector<pair<char,int>>::iterator Turno;
+int tamanio=0;
+bool start_game(){
+  if (LISTA_CLIENTES.size()==tamanio-1){
+    return true;
+  }
+  return false;
+}
+void EnviarMensaje(int socket_client, string mensaje){
+  int n = write(socket_client, mensaje.c_str(), mensaje.length());
+  if (n < 0) perror("ERROR writing to socket");
+}
+void BroadCast(string mensaje){
+  cout<<"mensaje a todos "<<mensaje<<endl;
+  for (auto &i : LISTA_CLIENTES){
+    EnviarMensaje(i.second, mensaje);
+  }
+}
+void Proceso_Thread(int socket_client){
+  string msgToChat;
+  char buffer[256];
+  int n;
+  char comando;
+  int longitud = 0;
+  do{ 
+    n = read(socket_client,buffer,1);     
+    if (n < 0) perror("ERROR reading from socket");
+    if(n==1){
+      BroadCast(buffer);
+    }
+    bzero(buffer, 256);
+  } while (true);
+  shutdown(socket_client, SHUT_RDWR);
+  close(socket_client); 
+}
+
+int main(void){
+  struct sockaddr_in stSockAddr;
+  int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if(-1 == SocketFD){perror("can not create socket");exit(EXIT_FAILURE);}
+  memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+  stSockAddr.sin_family = AF_INET;
+  stSockAddr.sin_port = htons(1100);
+  stSockAddr.sin_addr.s_addr = INADDR_ANY;
+  if(-1 == bind(SocketFD,(const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in))){perror("error bind failed");close(SocketFD);exit(EXIT_FAILURE);}
+  if(-1 == listen(SocketFD, 10)){perror("error listen failed");close(SocketFD);exit(EXIT_FAILURE);}
+
+  for(;;){
+    int ClientSD = accept(SocketFD, NULL, NULL);//ClientSD es el numero del socket
+      if(0 > ClientSD){perror("error accept failed");close(SocketFD);exit(EXIT_FAILURE);}
+      else{
+          char buffer[4];
+          int n=read(ClientSD,buffer,4);
+          tamanio=(int)(buffer[1])-48;
+          //tabla de tamanio del tablero -1
+          LISTA_CLIENTES.push_back(std::pair<char,int>(buffer[3],(int)ClientSD));
+          cout<<"Inserto cliente "<<buffer[3]<<(int)ClientSD<<endl;
+          cout<<"empieza juego "<<start_game()<<endl;
+          if(LISTA_CLIENTES.size()==tamanio-1){;
+            Turno=LISTA_CLIENTES.begin();
+            string pri(1,Turno->first);
+            BroadCast(pri+"");
+          }
+          std::thread(Proceso_Thread, ClientSD).detach();
+          Turno++;
+      }
+  }   
+  close(SocketFD);
+  return 0;
+}
